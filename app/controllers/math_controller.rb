fa77bfee0c81e919a5eda1c2a8269dc978394438
Finch Yuan <fyuan@chicagobooth.edu>
2023-04-13 16:21:11 +0000

    Done with addition

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -1,6 +1,14 @@
 class MathController< ApplicationController
+  
+  def addition
+    render({ :template => "math_templates/addition_form.html.erb" })
+  end
+  
   def add_results
-    
-  render({ :template => "math_templates/addition_result.html.erb" })
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @add_results = @first_num + @second_num
+
+    render({ :template => "math_templates/addition_result.html.erb" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
-  get("/wizard_add",{ :controller => "math", :action => "add_results"})
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get("/add",{ :controller => "math", :action => "addition"})
+
+  get("/wizard_add",{ :controller => "math", :action => "add_results"})
+
+
+  
 end
